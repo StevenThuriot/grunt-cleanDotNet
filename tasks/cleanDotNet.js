4632cbdd3947be76d7923e60f8b48873e44c81ec
Steven Thuriot <steven@thuriot.be>
2014-12-22 20:27:21 +0100

    Added vb.net support

--- tasks/cleanDotNet.js
+++ tasks/cleanDotNet.js
@@ -8,12 +8,12 @@
 
 'use strict';
 
-var path = require('path');
+var path = require('path'),
+    fs = require('fs');
 
-var walkSync = function (dir, filelist) {
+var walk = function (dir, filelist) {
         
-    var fs = fs || require('fs'),
-        files = fs.readdirSync(dir);
+    var files = fs.readdirSync(dir);
 
     filelist = filelist || [];
     
@@ -21,8 +21,8 @@ var walkSync = function (dir, filelist) {
         var fullname = path.join(dir, file);
         
         if (fs.statSync(fullname).isDirectory()) {
-            filelist = walkSync(fullname, filelist);
-        } else if (fullname.match(/\.csproj$/gi)) {
+            filelist = walk(fullname, filelist);
+        } else if (fullname.match(/\.csproj$|\.vbproj$/gi)) {
             filelist.push(fullname);
         }
     });
@@ -33,12 +33,11 @@ var walkSync = function (dir, filelist) {
 module.exports = function (grunt) {
     grunt.registerTask('cleanDotNet', 'Clean .NET build files', function () {
         var rimraf = require('rimraf'),
-            fs = require('fs'),
             cwd = process.cwd();
 
         console.log('- Cleaning ' + cwd);
 
-        walkSync(cwd).map(function (file) {
+        walk(cwd).map(function (file) {
             var directory = path.dirname(file);
             
             console.log('-   Found project folder: ' + directory);
