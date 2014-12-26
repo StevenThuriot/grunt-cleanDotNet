8c09280c9f80b9350c1d90098f3ba54de16616c4
Steven Thuriot <steven@thuriot.be>
2014-12-26 13:17:37 +0100

    Using my new walker lib instead!

--- tasks/cleanDotNet.js
+++ tasks/cleanDotNet.js
@@ -8,41 +8,38 @@
 
 'use strict';
 
+
+function folderStartsWith(stats, value) {
+    return stats.name.slice(0, value.length) == value;
+}
+
 var path = require('path'),
-    fs = require('fs');
-
-var walk = function (dir, filelist) {
-        
-    var files = fs.readdirSync(dir);
-
-    filelist = filelist || [];
-    
-    files.forEach(function (file) {
-        var fullname = path.join(dir, file);
-        
-        if (fs.statSync(fullname).isDirectory()) {
-            filelist = walk(fullname, filelist);
-        } else if (fullname.match(/\.csproj$|\.vbproj$/gi)) {
-            filelist.push(fullname);
+    fs = require('fs'),
+    rimraf = require('rimraf'),
+    walk = require('fs-walker'),
+    filter = {
+        file: function (stats) {
+            //Look for csproj and vbproj files to be sure, rather than looking for bin and obj folders.
+            return /\.csproj$|\.vbproj$/i.test(stats.name);
+        },
+        directory: function (stats) {
+            //Skip looking through big and uninteresting folders.
+            return stats.name[0] !== '.' && 
+                stats.name !== 'node_modules' && 
+                !folderStartsWith(stats, '_Resharper');
         }
-    });
-    
-    return filelist;
-};
+    };
 
 module.exports = function (grunt) {
     grunt.registerTask('cleanDotNet', 'Clean .NET build files', function () {
-        var rimraf = require('rimraf'),
-            cwd = process.cwd();
+        var cwd = process.cwd();
 
-        console.log('- Cleaning ' + cwd);
-
-        walk(cwd).map(function (file) {
-            var directory = path.dirname(file);
-            
-            console.log('-   Found project folder: ' + directory);
-            
-            return directory;
+        console.log('- Cleaning up %s', cwd);
+        var deletionCounter = 0;
+        
+        walk.sync(cwd, filter).map(function (stats) {
+            console.log('-   Found project folder: %s', stats.directory);
+            return stats.directory;
         }).map(function (dir) {
             var bin = path.join(dir, 'bin');
             var obj = path.join(dir, 'obj');
@@ -53,10 +50,11 @@ module.exports = function (grunt) {
         }).filter(function (dir) {
             return fs.existsSync(dir);
         }).forEach(function (dir) {
-            
-            console.log('-      Deleting ' + dir);
-            
+            console.log('-      Deleting %s', dir);
             rimraf.sync(dir);
+            deletionCounter++;
         });
+        
+        console.log('\n- Cleaned up %s folders.', deletionCounter);
     });
 };
